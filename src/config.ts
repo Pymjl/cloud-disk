c221eb0619e69810ed6dfe937d40f965905447e7
Zongzi <cs.zongzi@gmail.com>
2022-05-25 00:33:03 +0800

    DEV 环境设置为本地服务器地址

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,1 @@
-// FIXME 后端 API 地址，答辩前 DEV 环境应当设置为本地服务器地址
-export const API_URL = import.meta.env.DEV ? 'http://106.12.167.1:8976' : 'http://106.12.167.1:8976'
+export const API_URL = import.meta.env.DEV ? 'http://127.0.0.1:8976' : 'http://106.12.167.1:8976'
